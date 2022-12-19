ecd9d372ad0882a46bdf9dba3ef55305494ba119
momozer <zerouali.mi@gmail.com>
2022-12-19 17:00:37 +0100

    feat: creation du router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,7 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+    {path:'', component : HomeComponent },
+]
